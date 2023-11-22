4ae02b7dbce5a5d8335cee9160e94b6ee077b04f
ClaudiiaGutierrez <cgutierrez20533@iesjoanramis.org>
2023-11-22 09:08:19 +0100

    Ex12.py

--- ex11.py
+++ ex11.py
@@ -5,12 +5,60 @@ def menu_principal():
       2. Calculadora reals
       3. Sortir
       """)
-    x = int(input("Elegeixi una opció: "))
-    if x>0 and x<4:
-        return x
-    else:
-        return 0
+    a = int(input("Elegeixi una opció: "))
+    return a
+def calculadora_enters():
+    print("Hem passat per la calculadora d'enters")
 
+def calculadora_reals():
+    print("Hem passat per la calculadora de reals")
+       
 #Programa principal
-a = menu_principal()
-print(a) 
+a  = 1
+while a>0:
+    a = menu_principal()
+    match a:
+      case 1:
+        calculadora_enters()
+      case 2:
+        calculadora_reals()
+      case 3:
+        a = -1
+      case other:
+        print("Opció no vàlida!")
+        a =int("Elegeix una opció: ")
+        return a
+def calculadora_enters():
+   op = 1
+   while op<0:
+      print("""
+        Menú enters
+        1. Sumar
+        2. Restar
+        3. Multiplicar
+        4. Dividir
+        5. Sortir
+       """)
+      op = int(input("Elegeixi una opció: "))
+      match op:
+         case 1: # Sumar
+            x = int(input("Introdueixi el primer número: "))
+            y = int(input("Introdueixi el segon número: "))
+            print("{} + {} = {}".format(x, y, x+y))
+         case 2: # Restar
+            x = int(input("Introdueixi el primer número: "))
+            y = int(input("Introdueixi el segon número: "))
+            print("{} - {} = {}".format(x, y, x-y))
+          case 3: # Multiplicar
+            x = int(input("Introdueixi el primer número: "))
+            y = int(input("Introdueixi el segon número: "))
+            print("{} * {} = {}".format(x, y, x*y))
+          case 4: # Dividir
+            x = int(input("Introdueixi el primer número: "))
+            y = int(input("Introdueixi el segon número: "))
+            print("{} / {} = {}".format(x, y, x/y))
+          case 5: # Sortir 
+              print("Adéu, ja tornaràs a la calculadora inicial \n\n")
+              op=1
+def calculadora_reals():
+  
