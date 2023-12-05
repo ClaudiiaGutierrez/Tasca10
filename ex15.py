993720d8b464a3131f75144ddd58f5fd8e3eb3c7
ClaudiiaGutierrez <148210605+ClaudiiaGutierrez@users.noreply.github.com>
2023-12-05 13:19:25 +0100

    Add files via upload

---
+++ ex15.py
@@ -0,0 +1,11 @@
+#Defineix una Funció
+def la():
+    a=[1, 2, 7, 3, 10,] 
+    print(a)
+    print (len(a))
+    b=[ "Hola", "Que", "tal", ]
+    print(b)
+    print(len(b))
+
+#Programa Principal
+la()
