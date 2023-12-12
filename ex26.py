5c7aaca0a42e956b880be829d512e673c91a0c78
ClaudiiaGutierrez <148210605+ClaudiiaGutierrez@users.noreply.github.com>
2023-12-12 20:56:58 +0100

    Add files via upload

---
+++ ex26.py
@@ -0,0 +1,14 @@
+def filtrar_paraules(a, num):
+	b = list()
+	i = 0
+	for e in a:
+			if num < len(e):
+				b.append(e)
+				return b
+    	
+        	
+#Programa Principal
+x = ["adeu", "no", "Mallorca es mes gran que Menorca", "Taxi", "classe", "Gimnàs"]
+a = input("Indicar la longitud de les paraules que vols filtrar: ")
+y = filtrar_paraules(x,int(a))
+print("Les paraules de igual o més tamany de ", a, " són: ", y)
