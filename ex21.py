3cc53900a14f5be94f6e479a8b4b02ce71e20c14
ClaudiiaGutierrez <148210605+ClaudiiaGutierrez@users.noreply.github.com>
2023-12-12 08:41:53 +0100

    Add files via upload

---
+++ ex21.py
@@ -0,0 +1,11 @@
+def crear_repetits(a,b):
+	c = b*int(a)
+	return c
+
+#Programa Principal
+x = input("Introdueixi un número: ")
+y = input("Introdueixi un caràcter: ")
+print("El caràcter ", y, " repetit ",x,"-vegades és: ", crear_repetits(x,y))
+
+
+                   
