a29be134b277c052ae11509c75150e2f3a63f93e
ClaudiiaGutierrez <148210605+ClaudiiaGutierrez@users.noreply.github.com>
2023-12-15 08:20:58 +0100

    Add files via upload

--- ex55.py
+++ ex55.py
@@ -3,14 +3,14 @@ def dibuixar(a):
     l=[]
     for j in range(a+1):
         for i in range(j):
-            l.append('*')
-            print(' '.join(map(str,l)))
-            l.clear()
-            for j in range(a+1,0,-1):
+                 l.append('*')
+        print(' '.join(map(str,l)))
+        l.clear()
+    for j in range(a+1,0,-1):
                 for i in range(j):
-                    l.append('*')
-                    print(' '.join(map(str,l)))
-                    l.clear()
+                        l.append('*')
+                print(' '.join(map(str,l)))
+                l.clear()
 #Programa Principal
 a = int (input("Introdueixi un número: "))
 dibuixar(a)
